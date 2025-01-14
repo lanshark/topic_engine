947264cdd3f70accddbf460e2987936b3d0a22fd
Scott Sharkey <ssharkey@lanshark.com>
2025-01-14 00:09:07 -0500

    Final ajdustments to menu

--- core/static/js/menu.js
+++ core/static/js/menu.js
@@ -5,15 +5,8 @@ document.addEventListener('DOMContentLoaded', function () {
     const dropdownButton = document.getElementById('dropdownNavbarLink');
     const dropdownMenu = document.getElementById('dropdownNavbar');
 
-    const doubleDropdownButton = document.getElementById('doubleDropdownButton');
-    const doubleDropdown = document.getElementById('doubleDropdown');
-
     dropdownButton.addEventListener('click', () => {
         dropdownMenu.classList.toggle('hidden');
-    });
-
-    doubleDropdownButton.addEventListener('click', () => {
-        doubleDropdown.classList.toggle('hidden');
     });
 
     button.addEventListener('click', function () {
